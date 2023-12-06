2a89f9d3338beb99efeeb3c23a7e9401be355362
SBICZA01\A253638 <rivaldo.mkhola@standardbank.co.za>
2023-12-06 22:02:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song holiday = new Song("Holiday", "Green DAy", 12);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 23);
+
+            Console.WriteLine(Song.songCount);
+            Console.ReadLine();
+        }
+    }
+}
